2e05e8c7be38ebb3b7ee4b71507415348bcd186f
kokhapp <kokhapp@gmail.com>
2023-09-14 23:02:08 +0700

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed modi,
+        quisquam illum sequi excepturi beatae architecto ratione unde
+        exercitationem minus ad officiis reiciendis temporibus recusandae
+        quidem! Non atque distinctio possimus!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
