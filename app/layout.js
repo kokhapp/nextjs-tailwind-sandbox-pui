2d3d621d2da76eb57e81a33c2710743ed33a8789
kokhapp <kokhapp@gmail.com>
2023-09-15 00:58:11 +0700

    new update

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,17 @@
-import './globals.css'
-
+import MainLayout from "@/components/MainLayout";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Nextjs Tailwind Sandbox',
-  description: 'This is a tutorial project from DevEmpower YouTube channel',
-}
+  title: "Nextjs Tailwind Sandbox",
+  description: "This is a tutorial project from DevEmpower YouTube channel",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainLayout>{children}</MainLayout>
+      </body>
     </html>
-  )
+  );
 }
